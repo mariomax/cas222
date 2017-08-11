d873a2b66a0a3c7dd1d68f9cd4a98e275cfffdf2
mariomax <mario.lopriore@pcc.edu>
2017-08-10 17:21:11 -0700

    Merging this branch
    This wasn't exactly what I wanted to do here. Just merging and
    continuing on with the main branch...

--- contact/contact.html.php
+++ contact/contact.html.php
@@ -6,11 +6,11 @@
         <link rel="stylesheet" href="styles.css">
     </head>
         <form method="post" action="contact/index.php">
-            <label for="c_name">*Name:</label>
+            <label for="c_name">*Name:</label><br>
             <input type="text" name="c_name" id="c_name" required><br>
-            <label for="c_email">*Email:</label>
+            <label for="c_email">*Email:</label><br>
             <input type="text" name="c_email" id="c_email" required><br>
-            <label for="comment">Comments:</label>
+            <label for="comment">Comments:</label><br>
             <textarea rows="4" cols="50" type="text" name="comment" id="comment">
             </textarea><br>
             <input type="submit">
